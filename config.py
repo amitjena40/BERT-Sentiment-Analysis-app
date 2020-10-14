b7bef008280066d45c7301c0e1a0bfb5ba04a988
amitjena40 <amitjena40@gmail.com>
2020-10-14 13:16:01 +0530

    Adding project files

---
+++ config.py
@@ -0,0 +1,14 @@
+import transformers
+
+DEVICE = "cuda"
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 2
+BERT_PATH = "../input/bert-base-uncased/"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "../input/IMDB Dataset.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BERT_PATH,
+    do_lower_case = True
+)
